c86cd25e4a50c94539ee80135e8d35b83ae143b6
xJoeCode <xjosephtbr@gmail.com>
2022-12-28 19:34:18 +0800

    fixed custom validation not working as intended

--- src/Components/form/EducationInfoForm.js
+++ src/Components/form/EducationInfoForm.js
@@ -3,37 +3,63 @@ import {useForm} from 'react-hook-form'
 import SelectInput from "../Inputs/SelectInput";
 import Checkbox from "../Inputs/Checkbox";
 import Button from "../UI/Button";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect } from "react";
 
 
 
 export default function EducationInfoForm(props){
     const {register, watch, formState: { errors }, setError, clearErrors, handleSubmit} = useForm()
+    let {graduationStartDate, graduationEndDate, currentlyAttending} = watch()
 
 
-
-
+    
+    
+    
     useEffect(()=>{
-        console.log(errors)
         
+        
+        console.log(errors)
+
+
         
     })
+    
 
+    const customDateValidation = () =>{
+        
+        const dateOptions = { year:'numeric', month:'long', day:'numeric'}
+        graduationStartDate = new Date(`${graduationStartDate}`)
+        currentlyAttending ? graduationEndDate = new Date() : graduationEndDate =  new Date(`${graduationEndDate}`)
+        console.log(graduationStartDate < graduationEndDate)
+        console.log(graduationStartDate , graduationEndDate)
+        return(graduationStartDate <= graduationEndDate)
+    }
     
     const degreeOptions = ['High School Diploma', 'GED', 'Associate of Arts','Associate of Science', 'Associate of Applied Science', 'Bachelor of Arts', 'Bachelor of Science', 'BBA', 'Master of Arts', 'Master of Science', 'MBA', 'PH.D' ]
 
     const submitHandler = (data,e) =>{
         e.preventDefault()
+        
+         graduationStartDate = new Date(`${data.graduationStartDate}`)
+        currentlyAttending ?  graduationEndDate = new Date(): graduationEndDate = new Date(`${data.graduationEndDate}`)
+
+        if ( graduationStartDate >= graduationEndDate){
+            setError("graduationEndDate", { type: "custom" , message:'Graduation End Date should be later than the start date' })
+        } else {
+            clearErrors('graduationEndDate')
+        }
+        
+        
+        console.log(graduationStartDate < graduationEndDate)
+
         const dateOptions = { year:'numeric', month:'long', day:'numeric'}
-        //const {graduationStartDate, graduationEndDate} = data
-        data.graduationStartDate = new Date(`${data.graduationStartDate}`).toLocaleDateString('en-GB', dateOptions)
-        data.graduationEndDate = data.currentlyAttending ? new Date().toLocaleDateString('en-GB', dateOptions) : new Date(`${data.graduationEndDate}`).toLocaleDateString('en-GB', dateOptions)
-        data.graduationStartDate >= data.graduationEndDate  ? setError("graduationEndDate", { type: "custom" , message:'Graduation End Date should be later than the start date' }) : clearErrors("graduationEndDate")
-        const customValidation = data.graduationStartDate < data.graduationEndDate 
-        console.log(data.graduationStartDate > data.graduationEndDate)
-        console.log(errors)
-        console.log(data)
+        data.graduationStartDate = graduationStartDate.toLocaleDateString('en-GB', dateOptions)
+        data.graduationEndDate = graduationEndDate.toLocaleDateString('en-GB', dateOptions)
+
+        const customValidation = graduationStartDate < graduationEndDate 
         customValidation && props.educationFormData(data,props.id)
+        
+
     }
 
     const cancelHandler = (e) =>{
@@ -50,12 +76,17 @@ export default function EducationInfoForm(props){
             <form onSubmit={handleSubmit(submitHandler)} className="flex flex-col">
                 <ul>
                     <Input register={{...register('schoolName', {required:true, value:props.data?.schoolName})}} labelName="School Name" inputData={{ type: "text", id: "School Name", placeholder:"e.g James Cook Univeristy"  }}> </Input>
+                    {errors.schoolName &&  <p className="text-[#e04040] text-xs"> School Name field is missing</p>}
                     <Input register={{...register('schoolLocation', {required:true, value:props.data?.schoolLocation})}} labelName="School Location" inputData={{ type: "text", id: "School Name", placeholder:"e.g Singapore"  }}> </Input>
+                    {errors.schoolLocation &&  <p className="text-[#e04040] text-xs"> School Location field is missing</p>}
                     <SelectInput register={{...register('degree', {required:true, value:props.data?.degree})}} inputData={{name:'Degree', id:'degreeSelect', options:{degreeOptions}}}>Degree</SelectInput>
                     <Input register={{...register('graduationStartDate', {required:true, value:props.data?.graduationStartDate})}} labelName="Graduation Start Date" inputData={{ type: "date", id: "graduationStartDate"}}> </Input>
-                    {!watch('currentlyAttending') && <Input register={{...register('graduationEndDate', {required:true, value:props.data?.graduationEndDate})}} labelName="Graduation End Date" inputData={{ type: "date", id: "graduationEndDate"}}> </Input>}
-                    {errors?.graduationEndDate?.type === 'custom' && <p className="text-[#e04040] text-xs"> {`${errors.graduationEndDate.message}`}</p>}
+                    {errors.graduationStartDate && <p className="text-[#e04040] text-xs"> Graduation Start Date field is missing</p>}
+                    {watch('currentlyAttending') || <Input register={{...register('graduationEndDate', {required:true, value:props.data?.graduationEndDate})}} labelName="Graduation End Date" inputData={{ type: "date", id: "graduationEndDate"}}> </Input>}
+                    {errors.graduationEndDate?.type === 'custom' && <p className="text-[#e04040] text-xs"> {`${errors.graduationEndDate.message}`}</p>}
+                    {errors.graduationEndDate?.type === 'required' && <p className="text-[#e04040] text-xs"> Graduation End Date field is missing</p>}
                     <Checkbox register={{...register('currentlyAttending',{value:props.data?.currentlyAttending} )}} labelName="I currently attend here" inputData={{ type: "checkbox", id: "currentlyAttending"}}> </Checkbox>
+                    
                     <Button bgColor='bg-brightYellow' onClick={handleSubmit(submitHandler)}>Submit</Button>
                     <Button bgColor='bg-brightYellow' onClick={cancelHandler}>Cancel</Button>
                 </ul>
--- src/Components/form/WorkHistoryForm.js
+++ src/Components/form/WorkHistoryForm.js
@@ -1,6 +1,7 @@
 import Input from "../Inputs/Input";
 import {useForm} from 'react-hook-form'
 import SelectInput from "../Inputs/SelectInput";
+import { useEffect } from "react";
 import Checkbox from "../Inputs/Checkbox";
 import Button from "../UI/Button";
 
@@ -8,13 +9,26 @@ import Button from "../UI/Button";
 
 export default function WorkHistoryForm(props){
 
-    const {register, formState: { errors }, handleSubmit} = useForm()
+
+    const {register, formState: { errors }, setError, clearErrors, handleSubmit} = useForm()
+
+    useEffect(()=>{
+        console.log(errors)
+    })
 
     
     const submitHandler = (data,e) =>{
         e.preventDefault()
+        
+        const dateOptions = { year:'numeric', month:'long', day:'numeric'}
+
+        data.startDate = new Date(`${data.startDate}`).toLocaleDateString('en-GB', dateOptions)
+        data.endDate = data.currentlyWorking ? new Date().toLocaleDateString('en-GB', dateOptions) : new Date(`${data.endDate}`).toLocaleDateString('en-GB', dateOptions)
+        data.startDate >= data.endDate  ? setError("endDate", { type: "custom" , message:'End Date should be later than the start date' }) : clearErrors("endDate")
+        const customValidation = data.startDate < data.endDate 
         console.log(data)
-        props.workHistoryFormData(data,props.id)
+        console.log(data.startDate < data.endDate )
+        //customValidation && props.workHistoryFormData(data,props.id)
     }
 
     const cancelHandler = (e) =>{
@@ -33,6 +47,7 @@ export default function WorkHistoryForm(props){
                     <Input register={{...register('country', {required:true, value:props.data?.country})}} labelName="Country" inputData={{ type: "text", id: "country", placeholder:"e.g Singapore"  }}> </Input>
                     <Input register={{...register('startDate', {required:true, value:props.data?.startDate})}} labelName="Start Date" inputData={{ type: "date", id: "startDate"}}> </Input>
                     <Input register={{...register('endDate', {required:true, value:props.data?.endDate})}} labelName="End Date" inputData={{ type: "date", id: "endDate"}}> </Input>
+                    {errors.endDate?.type === 'custom' && <p className="text-[#e04040] text-xs"> {`${errors.graduationEndDate.message}`}</p>}
                     <Checkbox register={{...register('currentlyWorking',{value:props.data?.currentlyWorking} )}} labelName="I currently work here" inputData={{ type: "checkbox", id: "currentlyWorking"}}> </Checkbox>
                     <Button bgColor='bg-brightYellow' onClick={handleSubmit(submitHandler)}>Submit</Button>
                     <Button bgColor='bg-brightYellow' onClick={cancelHandler}>Cancel</Button>
